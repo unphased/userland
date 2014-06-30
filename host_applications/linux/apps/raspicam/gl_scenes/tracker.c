0f278febb96e289c2dfc238fd30449a00d946910
Steven Lu <stevenlu443@gmail.com>
2014-06-30 16:33:12 -0400

    adding the gl state stuff for a new scene for use with raspistill to build the first iteration of the GL powered image processing autopilot.

---
+++ host_applications/linux/apps/raspicam/gl_scenes/tracker.c
@@ -0,0 +1,14 @@
+/* 
+
+   Copyright (c) 2014, Steven Lu
+   All rights reserved.
+
+*/
+
+#include "RaspiTex.h"
+#include "RaspiTexUtil.h"
+#include <GLES2/gl2.h>
+#include <EGL/egl.h>
+#include <EGL/glext.h>
+
+
