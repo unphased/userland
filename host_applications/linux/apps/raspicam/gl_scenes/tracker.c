d067a6b5987ab697007d2a77041f5fe94603045b
Steven Lu <stevenlu443@gmail.com>
2014-07-01 21:40:27 -0400

    some more stuff

--- host_applications/linux/apps/raspicam/gl_scenes/tracker.c
+++ host_applications/linux/apps/raspicam/gl_scenes/tracker.c
@@ -9,6 +9,35 @@ All rights reserved.
 #include <EGL/egl.h>
 #include <EGL/glext.h>
 
+static RASPITEXUTIL_SHADER_PROGRAM_T tracker_shader = {
+   .vertex_source = 
+      "attribute vec2 vertex;\n"
+      "varying vec2 texcoord;\n"
+      "void main(void) {\n"
+      "  texcoord = vec2(0,0);\n"
+      "  gl_Position = vec4(vertex, 0.0, 1.0);\n"
+      "}\n",
+   .fragment_source = 
+      "#extension GL_OES_EGL_image_external : require\n"
+      "uniform samplerExternalOES tex;\n",
+   .uniform_names = {},
+   .attribute_names = {},
+};
+
+static int tracker_init(RASPITEX_STATE *state)
+{
+    int rc = raspitexutil_gl_init_2_0(state);
+    if (rc != 0)
+       goto end;
+
+    rc = raspitexutil_build_shader_program(&tracker_shader);
+end:
+    return rc;
+}
+
+static int tracker_redraw(RASPITEX_STATE * raspitex_state) {
+}
+
 int mirror_open(RASPITEX_STATE *state)
 {
    state->ops.gl_init = tracker_init;
